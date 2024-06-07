d6430a2106555b6eda77280060614c5f08eff028
DartiLab <admin@darti.ufma.br>
2024-06-07 11:37:43 -0300

    Add files via upload

---
+++ Códigos/Verifica_par(Pag 28).py
@@ -0,0 +1,17 @@
+
+print("======================")
+print("|Bem Vindo estudante!|")
+print("======================")
+print("\n\n")
+
+print("Aqui está um código para verificar se um numero é par ou não\n")
+
+num = int(input("Digite o numero que você deseja verificar:"))
+
+# utiliza-se de '%' para descobrir se a divisão de num por 2 é igual a 0, se sim prova-se que é par
+if (num % 2) == 0: #Lembrando == é para indicar igualdade e = para atribuir variavel
+    print("o numero é par")
+else:
+    print("o numero é impar")
+
+ 
